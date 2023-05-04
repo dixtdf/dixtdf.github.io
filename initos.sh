91477868bb06ba3416b41ec222845daadf161b42
dixtdf <106577535+dixtdf@users.noreply.github.com>
2023-05-04 10:19:04 +0800

    Create initos.sh

---
+++ initos.sh
@@ -0,0 +1,3 @@
+cp -a /etc/apt/sources.list /etc/apt/sources.list.bak
+sed -i "s@http://.*archive.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list
+sed -i "s@http://.*security.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list
