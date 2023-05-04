c78a288f201936f5d458e7f5f16766c43bd6fffb
dixtdf <106577535+dixtdf@users.noreply.github.com>
2023-05-04 11:11:30 +0800

    Update initos.sh

--- initos.sh
+++ initos.sh
@@ -2,7 +2,16 @@ cp -a /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i "s@http://.*archive.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list
 sed -i "s@http://.*security.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list
 apt update
-apt install -y vim curl openssh-server
+apt install -y vim curl openssh-server ca-certificates gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart sshd
 cp /etc/pam.d/gdm-password /etc/pam.d/gdm-password.bak
