90c8dc81d86213de1d6cfbfbc30a2f347bb19c5f
dixtdf <dixtdf@gmail.com>
2024-04-29 19:28:02 +0800

    change

--- ccsupportshortcut/static/js/theme_default/pre_main.js
+++ ccsupportshortcut/static/js/theme_default/pre_main.js
@@ -39,16 +39,16 @@ function setTheme(theme){
         }
         document.getElementsByTagName("html")[0].classList.add("dark");
         // load dark and light togher, distingush by .dark class instead use single css file
-        // removejscssfile("/static/css/theme_default/light.css", "css");
-        // addCss("/static/css/theme_default/dark.css");
+        // removejscssfile("/ccsupportshortcut/static/css/theme_default/light.css", "css");
+        // addCss("/ccsupportshortcut/static/css/theme_default/dark.css");
     }else{
         if(obj){
             obj.classList.remove("dark");
             obj.classList.add("light");
         }
         document.getElementsByTagName("html")[0].classList.remove("dark");
-        // removejscssfile("/static/css/theme_default/dark.css", "css");
-        // addCss("/static/css/theme_default/light.css");
+        // removejscssfile("/ccsupportshortcut/static/css/theme_default/dark.css", "css");
+        // addCss("/ccsupportshortcut/static/css/theme_default/light.css");
     }
     localStorage.setItem("theme", theme);
 }
